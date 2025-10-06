5971265ef0b848dfe7d2fdada61d799fad7b1d5a
klearxosx <klearxos.x@gmail.com>
2025-10-06 07:31:20 +0300

    feat: add HelloWorld

---
+++ src/gr/aueb/cf/helloApp/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.helloApp;
+
+/**
+ * This project was created to show how
+ * to make a git project. And then how to
+ * stage and commit changes in local repo.
+ * Then how to push to GitHub
+ */
+public class HelloWorld {
+}
