5f64c07d0e41a0912654f0e70d7626574ba5ea60
klearxosx <klearxos.x@gmail.com>
2025-10-07 09:07:44 +0300

    fix add app

--- src/gr/aueb/cf/helloApp/AddApp.java
+++ src/gr/aueb/cf/helloApp/AddApp.java
@@ -1,4 +1,14 @@
 package gr.aueb.cf.helloApp;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 public class AddApp {
+
+    public static void main(String[] args) {
+        System.out.println(add(3, 5));
+    }
+
+    public static  int add(int a, int b) {
+        return a + b;
+    }
 }
